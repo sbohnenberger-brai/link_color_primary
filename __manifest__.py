60fb42f9e1b46929d7735942f7dbe6b3c259f7c9
sbohnenberger-brai <163850228+sbohnenberger-brai@users.noreply.github.com>
2026-03-27 16:07:42 +0100

    Bump version from 19.0.4.0.0 to 19.0.5.0.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Brainson Odoo: Link Color Primary",
-    "version": "19.0.4.0.0",
+    "version": "19.0.5.0.0",
     "summary": "Force Odoo editor links to use your primary brand color",
     "category": "Tools",
     "author": "Custom",
