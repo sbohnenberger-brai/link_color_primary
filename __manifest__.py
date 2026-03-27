bbf57b16182aa70b407783e845e6ab42d5e3f3c1
sbohnenberger-brai <163850228+sbohnenberger-brai@users.noreply.github.com>
2026-03-27 15:56:09 +0100

    Update version and change SCSS to CSS in manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Brainson Odoo: Link Color Primary",
-    "version": "19.0.2.0.0",
+    "version": "19.0.3.0.0",
     "summary": "Force Odoo editor links to use your primary brand color",
     "category": "Tools",
     "author": "Custom",
@@ -9,7 +9,7 @@
     "assets": {
         "web.assets_backend": [
             "link_color_primary/static/src/js/link_color_primary.js",
-            "link_color_primary/static/src/scss/link_color_primary.scss",
+            "link_color_primary/static/src/css/link_color_primary.css",
         ],
         "web.assets_frontend": [
             "link_color_primary/static/src/js/link_color_primary.js",
