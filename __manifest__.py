a344a02e78bb2a437339745bc8f5890306cf0456
sbohnenberger-brai <163850228+sbohnenberger-brai@users.noreply.github.com>
2026-03-27 16:02:01 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Brainson Odoo: Link Color Primary",
-    "version": "19.0.3.0.0",
+    "version": "19.0.4.0.0",
     "summary": "Force Odoo editor links to use your primary brand color",
     "category": "Tools",
     "author": "Custom",
@@ -8,10 +8,6 @@
     "depends": ["web", "mail", "portal"],
     "assets": {
         "web.assets_backend": [
-            "link_color_primary/static/src/js/link_color_primary.js",
-            "link_color_primary/static/src/css/link_color_primary.css",
-        ],
-        "web.assets_frontend": [
             "link_color_primary/static/src/js/link_color_primary.js",
             "link_color_primary/static/src/css/link_color_primary.css",
         ],
