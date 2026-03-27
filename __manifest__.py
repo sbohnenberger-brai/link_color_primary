78713f3de6ffc76e7aa1fc150aec35260cdaf2e1
sbohnenberger-brai <163850228+sbohnenberger-brai@users.noreply.github.com>
2026-03-27 14:59:48 +0100

    Add manifest for Link Color Primary module

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,21 @@
-
+{
+    "name": "Link Color Primary",
+    "version": "19.0.1.0.0",
+    "summary": "Force Odoo editor links to use your primary brand color",
+    "category": "Tools",
+    "author": "Custom",
+    "license": "LGPL-3",
+    "depends": ["web", "web_editor", "mail", "portal"],
+    "assets": {
+        "web.assets_backend": [
+            "link_color_primary/static/src/js/link_color_primary.js",
+            "link_color_primary/static/src/scss/link_color_primary.scss",
+        ],
+        "web.assets_frontend": [
+            "link_color_primary/static/src/js/link_color_primary.js",
+            "link_color_primary/static/src/scss/link_color_primary.scss",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
